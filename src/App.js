b6a6a63b30576e8697e14b460dbf30596160449c
Amit Roy <roy932439@gmail.com>
2024-06-08 22:57:40 +0530

    Routing issues solve again

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     
     return (
         <div className={currentMode === 'Dark' ? 'dark' : ''}>
-            <BrowserRouter>
+            <BrowserRouter basename="/Admin_Dashboard">
                 <div className="flex relative dark:bg-main-dark-bg">
                     <div className="fixed right-4 bottom-4" style={{zIndex: "1000"}}>
                         <TooltipComponent content="Settings" poistion="Top">
@@ -52,7 +52,7 @@ const App = () => {
                     
                     <div>
                         {themeSettings && <ThemeSettings/>}
-                        <Routes>
+                        <Routes basename="/Admin_Dashboard">
                             {/* Dashboard */}
                             <Route path="/" element={<Ecommerce/>}/>
                             <Route path="/ecommerce" element={<Ecommerce/>}/>
